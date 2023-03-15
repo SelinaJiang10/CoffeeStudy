30ed5bab3e8a56129b0e92a579809f0984c12291
SelinaJiang10 <selinajiang10@gmail.com>
2023-03-14 22:18:18 -0400

    init html

---
+++ common/footer.js
@@ -0,0 +1,9 @@
+var footer = `
+<footer class="page-footer purple lighten-1">
+  <div class="footer-copyright">
+    <div class="container">
+    <p>Copyright © Selina Jiang</p>
+    </div>
+  </div>
+</footer>
+`
---
+++ common/nav.js
@@ -0,0 +1,23 @@
+var navbar = `
+<div id="navbar" class="navbar-fixed scrollspy">
+    <nav class="white">
+        <div class="nav-wrapper container">
+            <div class="container"> <a href="#navbar" class="brand-logo">Logo</a></div>
+
+            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+            <ul class="right hide-on-med-and-down">
+
+                <li><a class="head-link" href="index.html">Projects</a></li>
+                <li><a class="head-link" href="aboutus.html">About Us</a></li>
+                <li><a class="head-link" href="contactus.html">Contact Us</a></li>
+
+            </ul>
+            <ul class="right side-nav" id="mobile-demo">
+                <li><a class="head-link" href="index.html">Projects</a></li>
+                <li><a class="head-link" href="aboutus.html">About Us</a></li>
+
+                <li><a class="head-link" href="contactus.html">Contact Us</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>`
